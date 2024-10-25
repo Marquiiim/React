7043caf309e277e8f7fc4f6359f6fbc55cb06cae
Marcos Gabriel <marcosindev@gmail.com>
2024-10-25 16:14:28 -0300

    Testes React

---
+++ my-project/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+
+  
+
+  return (
+    <div className="App">
+      <h1>Olá React!</h1>
+      <p>Meu Primeiro APP</p>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
