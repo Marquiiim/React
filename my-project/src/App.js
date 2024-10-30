6486d344d1e49749e597c82b67bd190642cc7b55
Marcos Gabriel <marcosindev@gmail.com>
2024-10-30 17:10:27 -0300

    ReactRouters aprendizado

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,19 +1,33 @@
 import './App.css';
-import { useState } from 'react'
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Empresa from './pages/Empresa'
+import Contato from './pages/Contato'
 
 function App() {
-  const [nome, setNome] = useState()
 
   return (
     <div className="App">
-      <h1>
-        State Lift
-      </h1>
-    <SeuNome setNome={setNome} />
-    <Saudacao nome={nome} />
-    </div>
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/empresa">Empresa</Link>
+          </li>
+          <li>
+            <Link to="/contato">Contato</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/empresa" element={<Empresa />} />
+          <Route path="/contato" element={<Contato />} />
+        </Routes>
+      </Router >
+
+    </div >
   );
 }
 
